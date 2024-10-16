d1aa806362af90a049509ef7ecd820e8fc168b3f
RogerioZFaga <rogeriozfaga@gmail.com>
2024-10-16 19:08:13 -0300

    atualizando banco de dados

--- mahjongFingertips/mahjong.sql
+++ mahjongFingertips/mahjong.sql
@@ -1,25 +1,27 @@
 -- Criação do banco de dados
-CREATE DATABASE mahjong_fingertips;
+CREATE DATABASE mahjong;
 
 -- Usar o banco de dados criado
-USE mahjong_fingertips;
+USE mahjong;
 
 -- Tabela de Jogadores
 CREATE TABLE jogadores (
-    id INT IDENTITY(1,1) PRIMARY KEY, -- Chave primária autoincremental
+    id BIGINT IDENTITY(1,1) PRIMARY KEY, -- Chave primária autoincremental
     nome NVARCHAR(100) NOT NULL,      -- Nome do jogador
     pontuacao INT DEFAULT 0           -- Pontuação do jogador
 );
 
 -- Tabela de Partidas
 CREATE TABLE partidas (
-    id INT IDENTITY(1,1) PRIMARY KEY,  -- Chave primária autoincremental
-    jogador1_id INT NOT NULL,          -- ID do primeiro jogador
-    jogador2_id INT NOT NULL,          -- ID do segundo jogador
-    vencedor_id INT,                   -- ID do jogador vencedor
+    id BIGINT IDENTITY(1,1) PRIMARY KEY,  -- Chave primária autoincremental
+    jogador1_id BIGINT NOT NULL,          -- ID do primeiro jogador
+    jogador2_id BIGINT NOT NULL,          -- ID do segundo jogador
+    vencedor_id BIGINT,                   -- ID do jogador vencedor
     data_partida DATETIME DEFAULT GETDATE(),  -- Data da partida
     FOREIGN KEY (jogador1_id) REFERENCES jogadores(id),  -- Chave estrangeira
     FOREIGN KEY (jogador2_id) REFERENCES jogadores(id),  -- Chave estrangeira
     FOREIGN KEY (vencedor_id) REFERENCES jogadores(id)   -- Chave estrangeira
 );
+
+DROP TABLE jogadores
 
